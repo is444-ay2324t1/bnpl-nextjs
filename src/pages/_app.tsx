bd1178f21bc2d38124a5bdbad3e0ee99048a415b
KeithLoh <keithloh99@gmail.com>
2023-10-24 20:01:26 +0800

    feat: add basic navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+import { cn } from "@/utils/cn";
 
+export const inter = Inter({
+  subsets: ["latin"],
+});
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={cn(inter.className, "")}>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </main>
+  );
 }
