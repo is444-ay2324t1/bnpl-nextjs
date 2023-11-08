8461019ccc7e67220d34070872074f17254d3a8d
KeithLoh <keithloh99@gmail.com>
2023-11-08 15:49:16 +0800

    chore: code cleanup

--- src/components/PaymentDialog.tsx
+++ src/components/PaymentDialog.tsx
@@ -3,15 +3,12 @@ import {
   AlertDialogAction,
   AlertDialogCancel,
   AlertDialogContent,
-  AlertDialogDescription,
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Separator } from "@radix-ui/react-separator";
 import { Gift, X } from "lucide-react";
 
